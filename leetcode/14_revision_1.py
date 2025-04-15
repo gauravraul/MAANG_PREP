f029c218010f3de35dad369b2cf01bfe979d22ee
Gaurav Raul <raulgaurav96@gmail.com>
2025-04-16 00:36:44 +0530

    doing revision

---
+++ leetcode/14_revision_1.py
@@ -0,0 +1,6 @@
+# 1. Contains Duplictaes:
+    # just compare length of original list and set(list)
+
+# 2. Valid Anagrams:
+    # just check if length is not same, they are not anagrams
+    # if length is same, check if sorted(original) == sorted(anagram)
