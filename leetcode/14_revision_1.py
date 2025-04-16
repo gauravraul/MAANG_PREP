9df083680fbe983ea88cbaab1eb0fd10c4fbd22b
Gaurav Raul <raulgaurav96@gmail.com>
2025-04-17 00:16:41 +0530

    revised 2 more problems

--- leetcode/14_revision_1.py
+++ leetcode/14_revision_1.py
@@ -1,8 +1,21 @@
-# 1. Contains Duplictaes:
+# 1. Contains Duplicates: [Python]
     # just compare length of original list and set(list)
 
-# 2. Valid Anagrams:
+# 2. Valid Anagrams: [Python]
     # just check if length is not same, they are not anagrams
     # if length is same, check if sorted(original) == sorted(anagram)
 
+# =======================================================================
 # Currently above one's using python, later do it using DSA.
+# =======================================================================
+
+# 3. Two Sum: [DSA]
+    # store value -> index in dict{}, as you need to return indices
+    # value is nothing but the diff = target - num
+
+# 4. Valid Palindrome: [Python]
+    # Just check if reverse of the string is exactly same (just ignore spaces and case-sensitivity)
+    # newStr == newStr[::-1]
+
+
+
